af626beed69c539edfef4c4dea68334616013741
Ali Muhammad <ali.work@alimuhammad.co>
2020-08-20 13:58:09 -0400

    Updates

---
+++ precache-manifest.0038adde038798d388bd1cc9c6034351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d11f0005693dbb5a3ec809bc629234",
+    "url": "/Movies-app/index.html"
+  },
+  {
+    "revision": "9317dc6f5e9c25af7e0e",
+    "url": "/Movies-app/static/js/2.4fb504f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Movies-app/static/js/2.4fb504f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc77d9e89d07ff15e99a",
+    "url": "/Movies-app/static/js/main.d1d509d8.chunk.js"
+  },
+  {
+    "revision": "16f96fbc40fe045af0d8",
+    "url": "/Movies-app/static/js/runtime-main.f6c1b355.js"
+  }
+]);
